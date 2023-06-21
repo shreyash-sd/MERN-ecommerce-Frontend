f3277ed8dc85f974a53f0fb7515a85e4bf96b1e2
Shreyash Dawake <shreyashdawake1@gmail.com>
2023-06-21 21:13:11 +0530

    Feature: Cart Page added

--- src/features/navbar/Navbar.js
+++ src/features/navbar/Navbar.js
@@ -2,7 +2,7 @@
 import { React, Fragment } from 'react'
 import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { Bars3Icon, ShoppingCartIcon, XMarkIcon } from '@heroicons/react/24/outline'
-
+import { Link } from 'react-router-dom';
 const user = {
   name: 'Tom Cook',
   email: 'tom@example.com',
@@ -64,6 +64,7 @@ function Navbar({children}) {
                   </div>
                   <div className="hidden md:block">
                     <div className="ml-4 flex items-center md:ml-6">
+                      <Link to="/cart">
                       <button
                         type="button"
                         className="rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
@@ -72,6 +73,7 @@ function Navbar({children}) {
                         <ShoppingCartIcon className="h-6 w-6" aria-hidden="true" />
                         
                       </button>
+                      </Link>
                       <span className="inline-flex items-center rounded-md mb-7 -ml-3 bg-red-50 px-2 py-1 text-xs font-medium text-red-700 ring-1 ring-inset ring-red-600/10">
        3
       </span>
@@ -153,6 +155,7 @@ function Navbar({children}) {
                       <div className="text-base font-medium leading-none text-white">{user.name}</div>
                       <div className="text-sm font-medium leading-none text-gray-400">{user.email}</div>
                     </div>
+                    <Link to="/cart">
                     <button
                       type="button"
                       className="ml-auto flex-shrink-0 rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
@@ -162,6 +165,7 @@ function Navbar({children}) {
 
                       
                     </button>
+                    </Link>
                     <span className="inline-flex items-center rounded-md mb-7 -ml-3 bg-red-50 px-2 py-1 text-xs font-medium text-red-700 ring-1 ring-inset ring-red-600/10">
         3
       </span>
